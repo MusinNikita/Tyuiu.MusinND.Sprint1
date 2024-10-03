f79f1b9c38b4e1ecbf1f5d9de040b550a4cf1f93
Nikita Musin <Tyuiu.MusinN@outlook.com>
2024-10-03 23:36:45 +0500

    task3

---
+++ Tyuiu.MusinND.Sprint1.Task3.V10.Lib/DataService.cs
@@ -0,0 +1,16 @@
+﻿using tyuiu.cources.programming.interfaces.Sprint1;
+
+namespace Tyuiu.MusinND.Sprint1.Task3.V10.Lib
+{
+    public class DataService : ISprint1Task3V10
+    {
+        public double NumberToMoney(double number)
+        {
+            int rub = Convert.ToInt32(number);
+            int kop = Convert.ToInt32((number - rub) * 100);
+
+            return (double)(rub + kop);
+
+        }
+    }
+}
---
+++ Tyuiu.MusinND.Sprint1.Task3.V10.Test/UnitTest1.cs
@@ -0,0 +1,16 @@
+namespace Tyuiu.MusinND.Sprint1.Task3.V10.Test
+{
+    public class Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}
