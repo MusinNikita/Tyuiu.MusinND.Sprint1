feaed752c062d827d2399aaa92328286e80b296d
Nikita Musin <Tyuiu.MusinN@outlook.com>
2024-10-03 14:05:25 +0500

    Task1

---
+++ Tyuiu.MusinND.Sprint1.Task1.V6.Test/DataServiceTest.cs
@@ -0,0 +1,20 @@
+namespace Tyuiu.MusinND.Sprint1.Task1.V6.Test
+{
+    public class DataServiceTest
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void ValidExpression()
+        {
+            DataService ds = new DataService();
+            double x = 1.0;
+            double y = 2.0;
+            var res = ds.Calculate(x, y);
+            Assert.AreEqual(-1, res);
+        }
+    }
+}
---
+++ Tyuiu.MusinND.Sprint1.Task1.V6/Program.cs
@@ -0,0 +1,46 @@
+﻿using Tyuiu.MusinND.Sprint1.Task1.V6.Lib;
+
+namespace Tyuiu.MusinND.Sprint1.Task1.V6
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            DataService ds = new DataService();
+            Console.Title = "Спринт #1 | Выполнил: Мусин Никита Денисович | ИИПБ-24-1";
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* Спринт #1                                                               *");
+            Console.WriteLine("* Базовые навыки работы в C#                                              *");
+            Console.WriteLine("* Задание #1                                                              *");
+            Console.WriteLine("* Вариант #6                                                             *");
+            Console.WriteLine("* Выполнил: Мусин Никита Денисович | ИИПБ-24-1                            *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* УСЛОВИЕ:                                                                *");
+            Console.WriteLine("* Написать программу, которая запрашивает у пользователя исходные данные  *");
+            Console.WriteLine("* вычисляет результат по формуле (x + y) / (3 * y) и печатает его         *");
+            Console.WriteLine("* на экране.                                                              *");
+            Console.WriteLine("*                                                                         *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+            Console.WriteLine("*                                                                         *");
+            Console.WriteLine("* (x + y) / (3 * y)                                                       *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("*                                                                         *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+            Console.WriteLine("***************************************************************************");
+
+            double x, y;
+
+            Console.WriteLine("Введите число X:");
+            x = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine("Введите число Y:");
+            y = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine(ds.Calculate(x, y));
+            Console.ReadKey();
+        }
+    }
+}
