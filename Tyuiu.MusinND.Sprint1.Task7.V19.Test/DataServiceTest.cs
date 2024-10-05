9e4d8a92487f3deed9ab77f1a3bcc2d644fc629a
Nikita Musin <Tyuiu.MusinN@outlook.com>
2024-10-05 22:42:55 +0500

    Task7

---
+++ Tyuiu.MusinND.Sprint1.Task7.V19.Test/DataServiceTest.cs
@@ -0,0 +1,16 @@
+namespace Tyuiu.MusinND.Sprint1.Task7.V19.Test
+{
+    public class Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}
---
+++ Tyuiu.MusinND.Sprint1.Task7.V19/Program.cs
@@ -0,0 +1,34 @@
+﻿using Tyuiu.MusinND.Sprint1.Task7.V19.Lib;
+
+namespace Tyuiu.MusinND.Sprint1.Task7.V19
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            DataService ds = new DataService();
+            Console.Title = "Спринт #1 | Выполнил: Мусин Никита Денисович | ИИПб-24-1";
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* Спринт #1                                                               *");
+            Console.WriteLine("* Добавление к решению итоговых проектов по спринту                       *");
+            Console.WriteLine("* Задание #7                                                              *");
+            Console.WriteLine("* Вариант #19                                                             *");
+            Console.WriteLine("* Выполнил: Мусин Никита Денисович | ИИПб-24-1                            *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* УСЛОВИЕ:                                                                *");
+            Console.WriteLine("* Написать программу, которая вычисляет математическое выражение по       *");
+            Console.WriteLine("* исходным значениям данных, вводимых пользователем.                      *");
+            Console.WriteLine("*Ответ округлите до 3 знаков после запятой.                               *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+            Console.WriteLine("*z = x - ((7x^2) / (x^3)) + sin (x) + |x^4 - x^5|                         *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+            Console.WriteLine("***************************************************************************");
+
+            Console.WriteLine("Введите X:");
+            int x = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine(ds.Calculate(x));
+        }
+    }
+}
